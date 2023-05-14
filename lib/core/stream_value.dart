1285f9c4d233c43cd959c2b6b5950b10dce8cc45
eltonmorais <elton@marketinglucrativo.com>
2023-05-14 19:39:56 -0300

    :sparkles: Now we have error handle :sparkles: Now we have a onNullWidget to avoid boilerplate check on stream_value_builder :wrench: analysis_options changed for belluga default

--- lib/core/stream_value.dart
+++ lib/core/stream_value.dart
@@ -1,23 +1,39 @@
 import 'dart:async';
 
 class StreamValue<T> {
-  final T defaultValue;
+  late T _defaultValue;
 
-  StreamValue({required this.defaultValue});
-
-  final StreamController<T> _streamController = StreamController<T>.broadcast();
-
-  Stream<T> get stream => _streamController.stream;
-
-  T? _value;
-  T get value => _value ?? defaultValue;
-
-  addValue(T newValue){
-    _value = newValue;
-    _streamController.add(value);
+  StreamValue({T? defaultValue}) {
+    try {
+      _defaultValue = defaultValue as T;
+    } catch (e) {
+      throw Exception("if T is not-nullable, then default value is mandatory.");
+    }
   }
 
-  dispose(){
-    _streamController.close();
+  final _streamValueController = StreamController<T>.broadcast();
+  final _streamErrorMessageController = StreamController<String?>.broadcast();
+
+  Stream<T> get stream => _streamValueController.stream;
+
+  T? _value;
+  T get value => _value ?? _defaultValue;
+
+  String? _error;
+  String? get error => _error;
+
+  void addValue(T? newValue) {
+    _value = newValue;
+    _streamValueController.add(value);
+    _streamErrorMessageController.add(null);
+  }
+
+  void addError(String errorMessage) {
+    _streamErrorMessageController.add(errorMessage);
+  }
+
+  void dispose() {
+    _streamValueController.close();
+    _streamErrorMessageController.close();
   }
 }
--- lib/core/stream_value_builder.dart
+++ lib/core/stream_value_builder.dart
@@ -1,18 +1,18 @@
-import 'dart:async';
-
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/widgets.dart';
 import 'package:stream_value/main.dart';
 
 class StreamValueBuilder<T> extends StatelessWidget {
-  final StreamValue<T> streamValue;
+  final StreamValue<T?> streamValue;
+  final Widget? onNullWidget;
+  final Widget Function(String)? onErrorBuilder;
   final Widget Function(BuildContext, T) builder;
 
   const StreamValueBuilder({
     super.key,
     required this.streamValue,
     required this.builder,
+    this.onNullWidget,
+    this.onErrorBuilder,
   });
 
   @override
@@ -20,7 +20,26 @@ class StreamValueBuilder<T> extends StatelessWidget {
     return StreamBuilder(
       initialData: streamValue.value,
       stream: streamValue.stream,
-      builder: (context, snapshot) => builder(context, streamValue.value),
+      builder: (context, snapshot) {
+        final Widget? _onNullWidget = onNullWidget;
+
+        final bool _showNullWidget =
+            streamValue.value == null && _onNullWidget != null;
+
+        if (_showNullWidget) {
+          return _onNullWidget;
+        }
+
+        Widget Function(String)? _onErrorBuilder = onErrorBuilder;
+
+        final String? _error = streamValue.error;
+        final bool _showErrorWidget = _error != null && _onErrorBuilder != null;
+
+        if (_showErrorWidget) {
+          return _onErrorBuilder(_error);
+        }
+        return builder(context, streamValue.value as T);
+      },
     );
   }
 }
